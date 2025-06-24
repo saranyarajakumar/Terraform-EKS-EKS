14e607e154ad3f4872fd8cfe4417b04d5ba26d81
saranyarajakumar <121645385+saranyarajakumar@users.noreply.github.com>
2025-06-24 09:50:38 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket         = "devops-shack-s3"
     key            = "eks.terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "eu-west-2"
     
   }
 }
